ae06455565bd0e003705acfa78fc3a84ef6b78f0
Pierre GILLON <gillon.pierre69@gmail.com>
2019-09-05 10:18:06 +0200

    Power set of empty set return only the empty set.

---
+++ PowerSet-Kata/PowerSet-Kata/PowerSet.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PowerSet_Kata
+{
+    public class PowerSet
+    {
+        public int[][] Get(int[] set)
+        {
+            return new[] {new int[0]};
+        }
+    }
+}
--- PowerSet-Kata/PowerSet-Kata/UnitTest1.cs
+++ PowerSet-Kata/PowerSet-Kata/UnitTest1.cs
@@ -1,13 +1,24 @@
+using System.Linq;
 using Xunit;
 
 namespace PowerSet_Kata
 {
-    public class UnitTest1
+    public class PowerSetTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly PowerSet _powerset;
 
+        public PowerSetTests()
+        {
+            _powerset = new PowerSet();
+        }
+
+        [Fact]
+        public void power_set_of_0_element_return_empty_set()
+        {
+            var sets = _powerset.Get(new int[0]);
+
+            Assert.Single(sets);
+            Assert.Equal(new int[0], sets.Single());
         }
     }
 }
